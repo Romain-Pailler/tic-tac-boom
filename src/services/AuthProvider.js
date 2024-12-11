0ca5c13c0b4a69e399a22dafe91c224af8a27682
Gaetan66 <127093056+Gaetan66@users.noreply.github.com>
2024-12-11 17:42:16 +0100

    authprovider

---
+++ src/services/AuthProvider.js
@@ -0,0 +1,22 @@
+function isAuthenticated(){
+    const token = localStorage.getItem('token');
+    if(token){
+        return true;
+    }else{
+        return false;
+    }
+}
+
+function removeToken(){
+    if(isAuthenticated()){
+        localStorage.removeItem(token);
+    }
+}
+
+function setToken(token){
+    localStorage.setItem('token', token)
+}
+
+function getUserIdentity(){
+    
+}
--- src/services/httpClient.js
+++ src/services/httpClient.js
@@ -51,7 +51,7 @@ const request = async (
     const login = (username, password) => {
         return request('/login', 'POST', { username, password }, true);
         };
-        const register = (username, password) => {
+    const register = (username, password) => {
         return request('/register', 'POST', { username, password }, true);
         };
 
