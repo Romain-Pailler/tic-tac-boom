947af14f5cb531e9c962e6ed749c452ed296e595
Romain-Pailler <romain.pailler77@gmail.com>
2025-01-09 17:50:36 +0100

    ajout du nom sur le dashboard

--- src/components/Dashboard.vue
+++ src/components/Dashboard.vue
@@ -1,3 +1,16 @@
 <template>
-    Dashboard !
+    <p>Bonjour, {{username}} </p>
 </template>
+
+<script>
+import {getUserIdentity} from '@/services/AuthProvider.js'
+export default {
+        data() {
+            return {
+                username: getUserIdentity().username,
+            }
+        },
+        methods: {
+        }
+    }
+</script>
