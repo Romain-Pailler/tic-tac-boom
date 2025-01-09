878e93b43d8278c971669c9459011967f294592a
Gaetan66 <127093056+Gaetan66@users.noreply.github.com>
2025-01-09 17:26:11 +0100

    getuser

--- src/services/AuthProvider.js
+++ src/services/AuthProvider.js
@@ -1,3 +1,6 @@
+import {jwtDecode} from 'jwt-decode';
+
+
 function isAuthenticated(){
     const token = localStorage.getItem('token');
     if(token){
@@ -18,7 +21,14 @@ function setToken(token){
 }
 
 function getUserIdentity(){
-
+    const token = localStorage.getItem('token');
+    try{
+        const user = jwtDecode(token);
+        return user;
+    }catch(error){
+        console.error("erreur de decodage");
+        return null;
+    }
 }
 
 export { getUserIdentity, isAuthenticated, removeToken, setToken };
