90e96c805d98d3caa799dbe4bc01d2580619050b
Romain-Pailler <romain.pailler77@gmail.com>
2025-01-09 16:02:28 +0100

    ajout du login

--- src/components/Login.vue
+++ src/components/Login.vue
@@ -1,3 +1,35 @@
+<script>
+import { setToken } from '@/services/AuthProvider.js';
+import { login } from '@/services/httpClient.js';
+    export default {
+        data() {
+            return {
+                username: '',
+                password: '',
+                error: null
+            }
+        },
+        methods: {
+            async handleLogin(){
+                try {
+                    const token = await login(this.username,this.password)
+                    setToken(token)
+                } catch (err) {
+                    console.log(err)
+                    this.error = err.message || "Une erreur s'est produite lors de la connexion."
+                }
+            }
+        }
+    }
+</script>
+
 <template>
-    Login !! 
+    <h1>Login</h1>
+    <form @submit.prevent="handleLogin">
+        <input type="text" v-model="username" placeholder="Ecrivez votre nom d'utilisateur">
+        <input type="password" v-model="password" placeholder="Ecrivez votre mot de passe">
+        <button type="submit">Connexion</button>
+    </form>
+    <p v-if="error">{{this.error}}</p>
+    <p>{{username}} et  {{password}}</p>
 </template>
